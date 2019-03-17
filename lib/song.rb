cc6892b616926e7749ea05f895c1c184c21e7c16
Serena Berry <siberry226@gmail.com>
2019-03-17 09:56:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,42 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_title)
+    song = self.new
+    song.name = song_title
+    song
+  end
+  
+  def self.create_by_name(song_title)
+    song = self.create
+    song.name = song_title
+    song
+  end
+  
+  def self.find_by_name(name)
+    all.find { |song| song.name == name }
+  end
+  
+  def self.song_names
+    all.map do |song|
+      song.name
+    end
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|s| s.name}
   end
 
 end
